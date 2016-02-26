5725e8a0116510bfbaf9bf8405d95385a429b74b
Tiffany Yao <tiffany.yao1@gmail.com>
2016-02-26 15:10:27 -0500

    updated filtering/sorting

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -53,10 +53,13 @@ app.getAuthorInfo = function(){
 }
 
 
-
+//app.bookArray will be the array we will store all the authors books
+//app.allBookArray is the general array that will store the array of books passed on to display from the filter function to display and from initial ajax call to display
 // Function to choose author/get name by clicking pictures
 app.selectAuthor = function(){
 	$("label").on("click", function(e){
+		app.bookArray = [];
+		app.allBookArray = [];
 		e.preventDefault();
 		app.author = $(this).prev().val();
 		app.getAuthorID();
@@ -77,15 +80,11 @@ app.selectAuthor = function(){
 		app.getBookInfo;
 		$(".authorHeading h2").text(app.author);
 	});
-}
 
-//this will be the array we will store all the authors books
-app.allBookArray = [];
+}
 
 // Function to access list of author's books
 app.page = 1;
-//app.bookArray is the general array that will store the array of books passed on to display from the filter function to display and from initial ajax call to display - 
-app.bookArray = [];
 
 app.getBookList = function(){
 	$.ajax({
@@ -131,14 +130,14 @@ app.filterBooks = function(){
 	app.filteredBooksByRating = [];
 	console.log("entered filterBooks")
 	// console.log(app.rating);
-	app.bookArray.forEach(function(val, i){
-		console.log(val);
-		if ((val.average_rating >= app.rating) && (val.average_rating <= app.rating+(0.99))) {
-			console.log("enter if")
-			console.log(val.average_rating);
-			app.filteredBooksByRating.push(val);
-		}
-	})
+	// app.bookArray.forEach(function(val, i){
+	// 	console.log(val);
+	// 	if ((val.average_rating >= app.rating) && (val.average_rating <= app.rating+(0.99))) {
+	// 		console.log("enter if")
+	// 		console.log(val.average_rating);
+	// 		app.filteredBooksByRating.push(val);
+	// 	}
+	// })
 	console.log(app.filteredBooksByRating);
 	if (app.filteredBooksByRating.length === 0 ){
 		$("#books").empty();
@@ -147,16 +146,11 @@ app.filterBooks = function(){
 		$(".books h3").hide();
 		app.bookArray = app.filteredBooksByRating;
 		app.displayBooks()
+		app.sortBooks();
 	}
 
-	$("#showAll").on("click", function(e){
-		console.log("showall")
-		e.preventDefault();
-		$(".books h3").hide();
-		app.bookArray = app.allBookArray;
-		app.displayBooks();
-	})
-}
+
+};
 
 // Get author's bio information from their Goodreads profile
 app.getBookInfo = function(authorData){
@@ -186,16 +180,91 @@ app.displayBio = function() {
 	var bioTemplate = Handlebars.compile(bioHtml);
 	$("#authorBio").append(bioTemplate(app.bioInformation));
 }
-
+var $container;
 // Display list of author's books
 app.displayBooks = function(){
 	console.log("entered displayBooks")
-	$("#books").empty();
+	// $("#books").empty();
+
 	var bookHtml = $("#authorTemplate").html();
 	var bookTemplate = Handlebars.compile(bookHtml);
-	app.bookArray.forEach(function(data, i){
+   	app.bookArray.forEach(function(data, i){
 		$("#books").append(bookTemplate(data));
+	});
+   	
+	$container = $('#books').isotope({
+	    itemSelector: '.book-gallery',
+	    percentPosition: true,
+	   // layoutMode: 'vertical',
+	   	masonry: {
+	    	columnWidth: '.book-gallery'
+	   	},
+	    getSortData: {
+	    	name: '.name',
+		    number: function( bookRating ) {
+		        var rating = $( bookRating ).find('.rating span').text();
+		        console.log(rating);
+		        return parseFloat( rating.replace( /[\(\)]/g, '') );
+		    } //closes number
+		} //closes getSortData
+   	}); // closes $container
+
+   	$container.imagesLoaded().progress(function() {
+   		$container.isotope('layout');
+   	});
+
+}
+
+app.sortBooks = function(){
+
+	$("#sortByTitle").on("click", function(e){
+		e.preventDefault();
+		$container.isotope({ sortBy: 'name' })
 	})
+
+	$("#sortByRating").on("click", function(e){
+		e.preventDefault();
+		console.log("sort by rating click");
+		$container.isotope({ 
+			sortBy: 'number',
+			sortAscending: {
+				number: false
+			}
+		 });
+	});
+
+
+  	$('.star').on( 'click', function() {
+		if ($(this).hasClass("selected") && ($(this).hasClass("hover")) ) {
+			$(".star").not(".hover").removeClass("selected");
+		}
+		$(this).addClass("selected").prevAll().addClass("selected");
+    	var filterValue = $( this ).data('rating');
+    	console.log(filterValue);
+
+    	$container.isotope({
+     	 // filter element with numbers greater than 50
+    	  	filter: function() {
+	     	   	// _this_ is the item element. Get text of element's .number
+	      	  	var number = $(this).find('.rating span').text();
+	      	  	// return true to show, false to hide
+	        	return ( parseFloat( number ) >= filterValue )
+      		} //close filter fn
+    	}); //close $container
+    });
+
+    $("#showAll").on("click", function(e){
+    	e.preventDefault();
+    	console.log("showall")
+    	$(".books h3").hide();
+    	$(".star").removeClass("selected");
+    	//app.bookArray = app.allBookArray;
+    	//app.displayBooks();
+    	// app.sortBooks();
+    	$container.isotope({
+    	    filter: '*'
+    	});
+    });
 }
 
 // Reset button for authors
@@ -203,6 +272,8 @@ app.resetSearch = function() {
 	$(".reset").on("click",function(e) {
 		var $label = $("label");
 		e.preventDefault();
+		$container.isotope('destroy'); 
+		$('#books').empty();
 		$label.show();
 		$label.animate ({
 		}, function(){ $(this).removeAttr("style")} )
@@ -213,7 +284,6 @@ app.resetSearch = function() {
 		}, "slow");
 		$(".authorHeading").hide();
 	});
-	
 }
 
 // Hover effects for stars
@@ -225,18 +295,18 @@ app.starHover = function(){
 	.on("mouseleave", function(){
 		$(this).removeClass("hover").prevAll().removeClass("hover");
 	})
-	.on("click", function(e){
-		e.preventDefault();
-		app.rating = $(this).data("rating");
-		app.rating = parseInt(app.rating);
-		console.log(app.rating);
-		app.bookArray = app.allBookArray;
-		app.filterBooks();
-		if ($(this).hasClass("selected") && ($(this).hasClass("hover")) ) {
-			$(".star").not(".hover").removeClass("selected");
-		}
-		$(this).addClass("selected").prevAll().addClass("selected");
-	});
+	//.on("click", function(e){
+	//	e.preventDefault();
+	//	app.rating = $(this).data("rating");
+	//	app.rating = parseInt(app.rating);
+	//	console.log(app.rating);
+	//	app.bookArray = app.allBookArray;
+	//	app.filterBooks();
+	//	if ($(this).hasClass("selected") && ($(this).hasClass("hover")) ) {
+	//		$(".star").not(".hover").removeClass("selected");
+	//	}
+	//	$(this).addClass("selected").prevAll().addClass("selected");
+	//});
 };
 
 app.init = function(){
@@ -244,6 +314,7 @@ app.init = function(){
 	app.selectAuthor();
 	app.starHover();
 	app.resetSearch();
+	app.sortBooks();
 
 };
 
