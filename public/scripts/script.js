c20afdaee7e94d1c75ab09b5e8e28131bc8f0d3c
Tiffany Yao <tiffany.yao1@gmail.com>
2016-02-27 19:41:29 -0500

    fixed a display none issue causing the avg rating and pub year to not show and added bracket to displayBooks so it captures the isotope stuff

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -159,11 +159,8 @@ app.displayBio = function (bioInformation) {
 	$.each(bioInformation, function (i, info) {
 		var authorProfile = $("<p class='link-to-goodreads'>").html("<a href='" + bioInformation.GoodreadsResponse.author.link + "'>See her Goodreads profile</a>");
 		console.log(bioInformation.GoodreadsResponse.author.link);
-<<<<<<< HEAD
 		var authorHometown = $("<p class='hometown'>").html(bioInformation.GoodreadsResponse.author.hometown);
-=======
-		//var authorHometown = $("<p>").html(bioInformation.GoodreadsResponse.author.hometown);
->>>>>>> ba7216cb5020109389de6646affc874d32b87e3f
+
 		// Author's Goodreads bio
 		var weirdAbout = bioInformation.GoodreadsResponse.author.about;
 		// This is not secure and should not be used but we used it!
@@ -181,37 +178,40 @@ app.displayBooks = function (soManyBooks) {
 	$(".filters").show();
 	console.log(app.bookArray[8].publication_year);
 	$.each(app.bookArray, function (i, item) {
-		var title = $("<h2>").html(item.title);
-		var rating = $("<p class='rating'>").html("Average rating: <span>" + item.average_rating + "</span>");
+		var title = $("<h2>").addClass("name").html(item.title);
+		var rating = $("<p class='rating'>").append("Average rating: <span>" + item.average_rating + "</span>");
 		var publicationYear = $("<p class='publicationYear'>").html("Publication year: <span>" + item.publication_year + "</span>");
 		var image = $("<img>").attr("src", item.image_url);
+
 		var bookImage = $("<div>").addClass("book-image").append(image);
 		var bookSpecs = $("<div>").addClass("book-specs").append(title, rating, publicationYear);
+
 		var galleryItem = $("<div>").addClass("book-gallery").append(bookImage, bookSpecs);
+
 		$("#books").append(galleryItem);
 	});
+
+	$container = $('#books').isotope({
+		itemSelector: '.book-gallery',
+		percentPosition: true,
+		// layoutMode: 'vertical',
+		masonry: {
+			columnWidth: '.book-gallery'
+		},
+		getSortData: {
+			name: '.name',
+			year: '.publicationYear span',
+			number: function number(bookRating) {
+				var rating = $(bookRating).find('.rating span').html();
+				return parseFloat(rating.replace(/[\(\)]/g, ''));
+			} //closes number
+		} //closes getSortData
+	}); // closes $container
+
+	$container.imagesLoaded().progress(function () {
+		$container.isotope('layout');
+	});
 };
-
-$container = $('#books').isotope({
-	itemSelector: '.book-gallery',
-	percentPosition: true,
-	// layoutMode: 'vertical',
-	masonry: {
-		columnWidth: '.book-gallery'
-	},
-	getSortData: {
-		name: '.name',
-		year: '.publicationYear span',
-		number: function number(bookRating) {
-			var rating = $(bookRating).find('.rating span').text();
-			return parseFloat(rating.replace(/[\(\)]/g, ''));
-		} //closes number
-	} //closes getSortData
-}); // closes $container
-
-$container.imagesLoaded().progress(function () {
-	$container.isotope('layout');
-});
 
 app.sortBooks = function () {
 
@@ -233,6 +233,7 @@ app.sortBooks = function () {
 
 	$("#sortByPubYear").on("click", function (e) {
 		e.preventDefault();
+		console.log("sort by year");
 		$container.isotope({
 			sortBy: 'year',
 			sortAscending: {
@@ -257,8 +258,6 @@ app.sortBooks = function () {
 				return parseFloat(number) >= filterValue;
 			} //close filter fn
 		}); //close $container
-
-		app.noResults();
 	});
 
 	$("#showAll").on("click", function (e) {
@@ -268,20 +267,6 @@ app.sortBooks = function () {
 		$container.isotope({
 			filter: '*'
 		});
-	});
-};
-
-app.noResults = function () {
-
-	//arrayOfDisplayBlock = [];
-	$(".book-gallery").each(function () {
-		var displayStatus = $(this).css("display");
-		console.log(displayStatus);
-		if ($(this).css("display") === "block") {
-			console.log("displayblock");
-			//	arrayOfDisplayBlock.push($(this));
-		}
-		//console.log(arrayOfDisplayBlock);
 	});
 };
 
